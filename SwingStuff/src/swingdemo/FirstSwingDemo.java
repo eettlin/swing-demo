8218fdcac7898c8a09bcf4cc1bb1d10bd22a2eba
eettlin <mrumrocks@gmail.com>
2013-11-26 10:17:57 -0800

    Labels and text fields

--- SwingStuff/src/swingdemo/FirstSwingDemo.java
+++ SwingStuff/src/swingdemo/FirstSwingDemo.java
@@ -1,12 +1,26 @@
 package swingdemo;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class FirstSwingDemo {
 
 	public static void main(String[] args) {
 		// Create a JFrame, top level window
 		JFrame mainFrame = new JFrame("Title");
+
+		// main content pane: create and set
+		JPanel pnlContent = new JPanel();
+		mainFrame.setContentPane(pnlContent);
+
+		// naming conventions: prefix pnl, lbl, btn, txt, txtr, rdbtn, chbx, mn
+		JLabel lblInput = new JLabel("Please input your feelings");
+		pnlContent.add(lblInput);
+		
+		JTextField txtInput = new JTextField(20);
+		pnlContent.add(txtInput);
 
 		// Two ways to size:
 		// manual sizing: mainFrame.setSize(new Dimension(400, 200));
